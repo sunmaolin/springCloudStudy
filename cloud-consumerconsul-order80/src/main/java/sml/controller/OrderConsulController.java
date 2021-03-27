5fa3eb9c447aa7ab7b55c307bb9497018e31854b
sunmaolin <Mr.sun727@iCloud.com>
2021-03-27 23:32:48 +0800

    订单服务注册进Consul

---
+++ cloud-consumerconsul-order80/src/main/java/sml/controller/OrderConsulController.java
@@ -0,0 +1,28 @@
+package sml.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import sml.entities.CommonResult;
+import sml.entities.Payment;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderConsulController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String paymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL+"/payment/consul",String.class);
+        return result;
+    }
+
+}
