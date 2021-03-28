b8ce099492288daba635d81dfd41e3d9dbea67f0
sunmaolin <Mr.sun727@iCloud.com>
2021-03-28 23:44:04 +0800

    OpenFeign服务调用=Ribbon+RestTemplate（默认支持轮询负载均衡）

---
+++ cloud-consumer-feign-order80/src/main/java/sml/controller/OrderFeignController.java
@@ -0,0 +1,25 @@
+package sml.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import sml.entities.CommonResult;
+import sml.entities.Payment;
+import sml.service.PaymentFeignService;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderFeignController {
+
+    @Resource
+    private PaymentFeignService paymentFeignService;
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        return paymentFeignService.getPaymentById(id);
+    }
+
+}
---
+++ cloud-consumer-feign-order80/src/main/java/sml/service/PaymentFeignService.java
@@ -0,0 +1,17 @@
+package sml.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import sml.entities.CommonResult;
+import sml.entities.Payment;
+
+@Component
+@FeignClient(value = "CLOUD-PAYMENT-SERVICE")
+public interface PaymentFeignService {
+
+    @GetMapping("/payment/get/{id}")
+    CommonResult<Payment> getPaymentById(@PathVariable("id") Long id);
+
+}
