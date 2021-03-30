37f9ef832b83398c54695b3ea6b6d6c00d680551
sunmaolin <Mr.sun727@iCloud.com>
2021-03-30 22:17:09 +0800

    hystrix支付微服务降级fallback

--- cloud-provider-hystrix-payment8001/src/main/java/sml/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/sml/PaymentHystrixMain8001.java
@@ -2,8 +2,10 @@ package sml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class,args);
--- cloud-provider-hystrix-payment8001/src/main/java/sml/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/sml/service/PaymentService.java
@@ -1,5 +1,7 @@
 package sml.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,14 +21,24 @@ public class PaymentService {
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+            /*设置自身超时时间的峰值，峰值内可以正常运行，超过了需要有兜底的方法处理，作为服务降级fallback*/
+           @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_TimeOut(Integer id){
-        long timeNumber = 3000;
-        try {
-            Thread.sleep(timeNumber);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        long timeNumber = 5000;
+        // 无论是异常还是超时都会触发降级
+//        try {
+//            Thread.sleep(timeNumber);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        int age = 10/0;
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOut: "+id+"\t"+"O(∩_∩)O哈哈~"+" 耗时"+timeNumber+"秒钟";
+    }
+
+    public String paymentInfo_TimeOutHandler(Integer id){
+        return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOutHandler: "+id+"\t"+"/(ㄒoㄒ)/~~";
     }
 
 }
