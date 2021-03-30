da8df9c8c6cbb8cff913516553b09e2a82df6970
sunmaolin <Mr.sun727@iCloud.com>
2021-03-30 22:31:00 +0800

    hystrix订单微服务降级fallback（建议在客户端做降级）

--- cloud-consumer-feign-hystrix-order80/src/main/java/sml/controller/PaymentHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/sml/controller/PaymentHystrixController.java
@@ -1,5 +1,7 @@
 package sml.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +24,17 @@ public class PaymentHystrixController {
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallBackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+    })
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        int age = 10/0; //测试异常会不会降级
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
         return result;
+    }
+
+    public String paymentTimeOutFallBackMethod(Integer id){
+        return "我是消费者80，对方支付系统繁忙请10秒钟后再试或者自己运行出错检查自己";
     }
 
 }
