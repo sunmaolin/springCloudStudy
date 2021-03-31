e6f405f3a80c8fb96af83fde29e4867cf09db559
sunmaolin <Mr.sun727@iCloud.com>
2021-03-31 23:51:57 +0800

    hystrix服务熔断（熔断后不会走主逻辑，直接降级）

--- cloud-provider-hystrix-payment8001/src/main/java/sml/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/sml/service/PaymentService.java
@@ -1,8 +1,10 @@
 package sml.service;
 
+import cn.hutool.core.util.IdUtil;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Service
 public class PaymentService {
@@ -39,6 +41,31 @@ public class PaymentService {
 
     public String paymentInfo_TimeOutHandler(Integer id){
         return "线程池："+Thread.currentThread().getName()+" paymentInfo_TimeOutHandler: "+id+"\t"+"/(ㄒoㄒ)/~~";
+    }
+
+    // ------------- 服务熔断  -----------
+
+    @HystrixCommand(fallbackMethod = "paymentCircuitBreaker_fallback",commandProperties = {
+            //1.首先开启断路器
+            @HystrixProperty(name = "circuitBreaker.enabled",value = "true"),//是否开启断路器
+            //2.在快照时间窗口：断路器确定是否打开需要统计一些请求和错误数据，而统计的时间范围就是快照时间窗，默认为最近的十秒钟
+            @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds",value = "10000"),//时间窗口期
+            //3.窗口期内，请求峰值达到10次，若没到十次不会触发（默认十秒内20个请求）
+            @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold",value = "10"),//请求次数
+            //4.10次请求失败率达到60%。断路器close->open（默认10秒内超过50%的请求失败）
+            @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage",value = "60"),//失败率达到多少次后跳闸
+            //5. 一段时间后（默认5秒），这个时候断路器是半开状态close->halfOpen,会让其中一个请求进行转发，如果成功，断路器关闭
+    })
+    public String paymentCircuitBreaker(Integer id){
+        if(id < 0){
+            throw new RuntimeException("*****id  不能负数");
+        }
+        String serialNumber = IdUtil.simpleUUID();
+        return Thread.currentThread().getName() + "\t" + "调用成功，流水号：" + serialNumber;
+    }
+
+    public String paymentCircuitBreaker_fallback(Integer id){
+        return "id 不能为符属，请稍后再试。id：" + id;
     }
 
 }
