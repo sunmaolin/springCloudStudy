a2a2ec41cd39d9e3f9b79a80f61a8eb2ab78b53b
sunmaolin <Mr.sun727@iCloud.com>
2021-04-02 23:18:48 +0800

    Gateway网关断言等配置Predicate，匹配规则

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,9 @@
+import java.time.ZonedDateTime;
+
+public class T2 {
+    public static void main(String[] args) {
+        // 获取after参数的时间
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}
