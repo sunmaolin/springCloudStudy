dd43d0493e899e56810f75aa9a1bb4ece1b95f46
sunmaolin <Mr.sun727@iCloud.com>
2021-04-11 23:11:44 +0800

    支付服务注册进Nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/sml/PaymentControllerMain.java
@@ -0,0 +1,13 @@
+package sml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentControllerMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentControllerMain.class, args);
+    }
+}
