05ef5a1431dfe3ec84123e963b64d22cb4d13358
sunmaolin <Mr.sun727@iCloud.com>
2021-04-18 22:43:14 +0800

    sentinel热点限流

--- cloudalibaba-sentinel-service8401/src/main/java/sml/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/sml/controller/FlowLimitController.java
@@ -1,8 +1,11 @@
 package sml.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -31,6 +34,27 @@ public class FlowLimitController {
         }
         log.info("testD 测试RT");
         return "-----testD";
+    }
+
+    /**
+     * 热点限流,注意blockHandler的兜底方法只是在sentinel控制台违规的兜底方法，异常等不管
+     * @param p1
+     * @param p2
+     * @return
+     */
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2){
+        return "--------testHotKey";
+    }
+
+    /**
+     * 兜底方法
+     * @return
+     */
+    public String deal_testHotKey(String p1, String p2, BlockException blockException){
+        return "deal_testHotKey";
     }
 
 }
